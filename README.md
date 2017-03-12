65e3cb56c3f9d0ff5e217e61f97cb48c39c9e066
Hasib Sarvari <tkdparallax@gmail.com>
2017-03-12 20:58:47 +0800

    Updated README.md and uploaded the new scripts

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 Workshop run by Robin Chew, Hasib Sarvari for Hacker Pals
 
-In this awesome workshop we cover the basics of HTML5 & CSS3 to get you started on your next project
+In this awesome workshop we cover the basics of Python Programing to get you started on your next project
 
 This is in a series of workshops that will be connected throughout the year.
+
+
+
+
+###This work is licensed under a [Creative Commons Attribution-NonCommercial 4.0 International License.](https://creativecommons.org/licenses/by-nc-nd/4.0/)
--- source/python_expectation.py
+++ source/python_expectation.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python
+
+while True:
+    try:
+        x = int(raw_input("Please enter a number: "))
+        break
+    except ValueError:
+        print("Oops!  That was no valid number.  Try again...")
+
+"""
+
+Zero divison example
+
+"""
+def input_numbers():
+    a = float(input("Enter first number:"))
+    b = float(input("Enter second number:"))
+    return a, b
+
+
+x, y = input_numbers()
+
+while True:
+    try:
+        print("%d / %d is %f" % (x, y, x/y))
+        break
+    except ZeroDivisionError:
+       print("Cannot divide by zero")
+       x, y = input_numbers()
